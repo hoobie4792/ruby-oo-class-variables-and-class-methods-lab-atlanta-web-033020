dd706d3f7a166deca136ca95b9e9a563669621c1
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-31 21:55:23 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+  @@count = 0
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count += 1
+    if 
+  end
+  
+  def self.count
+    @@count
+  end
+end
